2ad91d348111bfadefe09cb461b600a7056bfe8b
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-10-29 17:51:49 +0100

    Component boilerplate file

---
+++ src/components/Boilerplate.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Boilerplate extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+Boilerplate.propTypes = {
+
+};
+
+export default Boilerplate;
