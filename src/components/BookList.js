24e4e12dddb8a5eeb72aec297f44bf4dcd859ac0
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-10-30 15:50:18 +0100

    Book catalogue cards styling

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -12,7 +12,25 @@ class BookList extends React.Component {
           <li>
             <div>
               <div className="content">
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                <img className="cover-img" src="https://covers.openlibrary.org/b/id/6995592-L.jpg"/>
+                <p className="card-title">
+                  Title
+                </p>
+                <p className="writter">
+                  by <span>Slobodan Djordjevic</span>
+                </p>
+                <br/>
+                <p className="desc">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                </p>
+                <div className="content-footer">
+                  <span className="price">
+                    $24.44
+                  </span>
+                  <button className="black-btn">
+                    Add to cart
+                  </button>
+                </div>
               </div>
             </div>
           </li>
